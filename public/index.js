04a4410c7507c5facf0ff3a6d23a5dac7cccc307
PianoMan51 <nikolajwold@hotmail.com>
2024-09-02 11:52:18 +0200

    total counter

--- public/index.js
+++ public/index.js
@@ -719,7 +719,7 @@ function updateMonthCharts(category) {
 
   
   document.querySelector(".doughnut.profits").innerHTML = "$" + monthTotalProfit.toFixed(0);
-  document.querySelector(".doughnut.sub.ratio").innerHTML = `${positives}/${negatives}`
+  document.querySelector(".doughnut.sub.ratio").innerHTML = `${positives}/${negatives}/${positives+negatives}`
 
   let sortedData = Object.entries(monthBarData).sort((a, b) => b[1] - a[1]);
 
